7cbc2bf9612eb3844ff0cd84b7800dcb479a8939
Alexander Morosow <alexander.morosow@gmail.com>
2023-12-22 17:13:02 +0100

    initial commit

---
+++ docker-entrypoint-initdb.d/init-mongo.js
@@ -0,0 +1,16 @@
+db = connect(
+  'mongodb://' +
+    process.env.MONGO_INITDB_HOSTNAME +
+    ':27017/' +
+    process.env.MONGO_INITDB_DATABASE,
+)
+db.createUser({
+  user: process.env.MONGO_INITDB_USERNAME,
+  pwd: process.env.MONGO_INITDB_PASSWORD,
+  roles: [
+    {
+      role: 'readWrite',
+      db: process.env.MONGO_INITDB_DATABASE,
+    },
+  ],
+})
